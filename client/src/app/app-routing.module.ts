69a7799bd9f080e1e7e687f3a45b01932afb266a
Sara Acuna <acunabenavidessaraelis@gmail.com>
2023-11-01 23:16:18 -0500

    diseño de gestion empleados

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { LoginComponent } from './components/login/login.component';
 import { PaymentManagementComponent } from './components/payment-management/payment-management.component';
 import { PaymentListComponent } from './components/payment-list/payment-list.component';
 import { EmployeeListComponent } from './components/employee-list/employee-list.component';
+import { ManageUserComponent } from './components/manage-user/manage-user.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/login', pathMatch: 'full' },
@@ -16,7 +17,7 @@ const routes: Routes = [
     { path: 'exit-payments', component: PaymentListComponent },
     ]
   },
-  { path: 'employees', component: EmployeeListComponent },
+  { path: 'employees', component: ManageUserComponent },
 
 ];
 
